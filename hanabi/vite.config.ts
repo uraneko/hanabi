d230e50e11af4034484f7bdb06e0145768877561
uraneko <BOOTHAlessandra@tutanota.com>
2025-12-26 20:05:48 +0100

    set static output asset file names in vite build script

--- hanabi/vite.config.ts
+++ hanabi/vite.config.ts
@@ -2,11 +2,30 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+	plugins: [solidPlugin()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext',
+		outDir: "build",
+		rollupOptions: {
+			output: {
+				assetFileNames: (file) => {
+					if (file.names[0] == "index.css") {
+						return 'assets/styles.css';
+					} else if (file.names[0] == "hanabi.svg") {
+						return 'assets/favicon.svg';
+					} else {
+						return `assets/[name]-[hash].[ext]`
+					}
+				},
+				entryFileNames: (file) => {
+					return `assets/bundle.js`
+				}
+
+
+			}
+		}
+	},
 });
